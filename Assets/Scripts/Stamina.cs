1077a46dece75c6f75757a5a81292fd2d9c2370a
Brendan-PC\Brendan <bmantheman@hotmail.com>
2020-09-30 21:17:53 -0600

    Added stamina

---
+++ Assets/Scripts/Stamina.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace Mechanarchy.Stats
+{
+    public class Stamina : MonoBehaviour
+    {
+        [SerializeField] float stamina = 100f;
+        [Tooltip("Stamina Points/s")][SerializeField] float staminaRecoverRate = 20f;
+        [SerializeField] float recoverDelay = 1f;
+
+        float delayTimer = 0f;
+        float maxStamina;
+
+        private void Awake()
+        {
+            maxStamina = stamina;
+        }
+
+        private void Update()
+        {
+            ProcessTimers();
+        }
+
+        private void ProcessTimers()
+        {
+            delayTimer += Time.deltaTime;
+            if (delayTimer >= recoverDelay)
+            {
+                if (stamina < maxStamina)
+                {
+                    stamina += staminaRecoverRate * Time.deltaTime;
+                    if (stamina > maxStamina)
+                    {
+                        stamina = maxStamina;
+                    }
+                }
+            }
+        }
+
+        public bool CanPerformAction(float staminaCost)
+        {
+            delayTimer = 0f;
+            if (stamina >= staminaCost)
+            {
+                stamina -= staminaCost;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
